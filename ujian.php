534eaf2d7ffea2397c4f5e0c418e24efa0d18a5a
xero <chansenku0@gmail.com>
2023-12-18 10:52:09 +0700

    ujian

---
+++ ujian.php
@@ -0,0 +1,18 @@
+<?php
+// echo "<p>saya mengerjakan ujian ini dengan jujur!</p>";
+
+// echo "<img src='gambar.jpg' alt='deskripsi'>";
+
+// echo "<h2>programming</h2>";
+
+// jadikan komentar berikut menjadi komentar multi baris php
+/*
+"sedang mengerjakan soal!"
+"dilarang berisik, ada ujian"
+"tetap fokus dan konsentrasi saat belajar, hindari gangguan dan periksa jadwal secara rutin"
+*/
+
+// echo "<textarea placeholder='sedang mengerjakan soal!' ></textarea>";
+
+// echo "<style>p {font-size: 50%; color: green;}</style>";
+?>
